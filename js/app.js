0daaac6f5d2da86dc3eabfacb4ffd0b76c247699
FabioLongoni <longoni.fabio90@gmail.com>
2022-07-22 16:37:31 +0200

    end-ex

--- js/app.js
+++ js/app.js
@@ -4,18 +4,15 @@ console.log('hello')
 for ( let i = 0 ; i < 100; i++) {
 
     const number = i + 1 ;
-
-    if (number % 3 === 0 && number % 5 === 0) {
-        console.log(number + 'FizzBuzz');
-    } else if (number % 3 === 0) {
-        console.log(number + 'Fizz');
-    } else if (number % 5 === 0) {
-        console.log(number + 'Buzz');
-    }
-
     
-
-
-
-
-}
+    if (number % 3 === 0 && number % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (number % 3 === 0) {
+        console.log('Fizz');
+    } else if (number % 5 === 0) {
+        console.log('Buzz');
+    }else if (number % 3 !== 0 && number % 5 !== 0) {
+        console.log(number)
+    }
+    
+}       
